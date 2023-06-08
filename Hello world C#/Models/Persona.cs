dcd6b0dcf7270733d0ab326da5cdc389ec879d56
Jonnier <jonnierosjitas@gmail.com>
2023-06-08 09:43:13 -0500

    Ejercicio 8 Clases y objetos ready

---
+++ Hello world C#/Models/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world_C_.Models
+{
+    internal class Persona
+    { 
+        public Persona(string Nombre, int Edad, string Profesion)
+        {
+            this.Nombre = Nombre;
+            this.Edad = Edad;
+            this.Profesion = Profesion;
+        }
+        public string Nombre { get; set;}
+        public int Edad { get; set;}
+        public string Profesion { get; set;}
+    }
+}
--- Hello world C#/Program.cs
+++ Hello world C#/Program.cs
@@ -1,12 +1,16 @@
-﻿using programs.funciones;
+﻿using Hello_world_C_.Models;
 namespace MyApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            funciones funciones = new funciones();
-            funciones.Ejercicio7();
+            Persona Jonnier = new Persona("Jonnier", 18, "Developer");
+            Persona Hanzeel = new Persona("Hanzeel", 14, "Musico");
+            Persona Carlos = new Persona("Carlos", 25, "Creativo");
+            Console.WriteLine(
+                $"{Jonnier.Nombre} {Hanzeel.Nombre} {Carlos.Nombre}"
+            );
             
         }
     }
